ee7e012583b6b41476c57f0d40bcd101d5db3360
XDLoLiK <66733765+XDLoLiK@users.noreply.github.com>
2022-05-08 20:39:11 +0300

    Add files via upload

---
+++ Inc/Utils/maths.h
@@ -0,0 +1,9 @@
+#ifndef Man_MATHS_H 
+#define Man_MATHS_H
+
+#include <inttypes.h>
+
+int64_t max(int64_t first, int64_t second);
+int64_t min(int64_t first, int64_t second);
+
+#endif // Man_MATHS_H
