399cc5074f1d44ce005999e25e8c6d08436bd1a2
XDLoLiK <66733765+XDLoLiK@users.noreply.github.com>
2022-05-20 12:35:18 +0300

    Add files via upload

---
+++ Src/Parser/frontend.c
@@ -0,0 +1,88 @@
+#include "parser.h"
+
+int Frontend_Main(const char *fileName)
+{
+	struct Parser *parser = Parser_New(fileName);
+	if (parser == NULL) {
+		return 1;
+	}
+
+	struct Tree_Node* syntaxTreeRoot = Parser_GetGrammar(parser);
+	if (syntaxTreeRoot == NULL) {
+		return 1;
+	}
+
+	char *treeFileName = calloc(strlen(fileName) + 6, sizeof (char));
+	if (treeFileName == NULL) {
+		return 1;
+	}
+	strcpy(treeFileName, fileName);
+	strcat(treeFileName, ".tree");
+	
+	FILE *treeFile = fopen(treeFileName, "w");
+	if (treeFile == NULL) {
+		return 1;
+	}
+	Tree_Upload(syntaxTreeRoot, treeFile, 0);
+
+	free(treeFileName);	
+	fclose(treeFile);
+	parser = Parser_Delete(parser);
+	syntaxTreeRoot = Tree_Delete(syntaxTreeRoot);
+	return 0;
+}
+
+int Frontend_DumpTokens(const char *fileName)
+{
+	struct Tokenizer *tok = Tokenizer_New(fileName);
+	if (tok == NULL) {
+		return 1;
+	}
+	struct Token *currentToken = Tokenizer_GetNextToken(tok);
+
+	char *tokenFileName = calloc(strlen(fileName) + 8, sizeof (char));
+	if (tokenFileName == NULL) {
+		return 1;
+	}
+	strcpy(tokenFileName, fileName);
+	strcat(tokenFileName, ".tokens");
+
+	FILE *tokenFile = fopen(tokenFileName, "w");
+	if (tokenFile == NULL) {
+		return 1;
+	}
+
+	for ( ; currentToken->type != TOKEN_TYPE_Endmarker; ) {
+		switch (currentToken->type) {
+
+			case TOKEN_TYPE_Number:
+				fprintf(tokenFile, "NUMBER:    %" PRId64 "\n", currentToken->number);
+				break;
+
+			case TOKEN_TYPE_String:
+				fprintf(tokenFile, "STRING:    %s\n", currentToken->string);
+				break;
+
+			case TOKEN_TYPE_Identifier:
+				fprintf(tokenFile, "IDENTIFER: %s\n", currentToken->identifier);
+				break;
+
+			case TOKEN_TYPE_Operator:
+				fprintf(tokenFile, "OPERATOR:  %d\n", currentToken->operator);
+				break;
+
+			default:
+				fprintf(tokenFile, "ERROR");
+				break;
+		}
+
+		currentToken = Tokenizer_GetNextToken(tok);
+		if (currentToken == NULL) {
+			Man_PrintTokError(tok);
+		}
+	}
+	fprintf(tokenFile, "END\n");
+
+	tok = Tokenizer_Delete(tok);
+	return 0;
+}
