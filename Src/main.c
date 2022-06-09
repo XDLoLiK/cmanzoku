849e0231e792b6f90165cb298f0a722157055f2c
XDLoLiK <66733765+XDLoLiK@users.noreply.github.com>
2022-06-09 22:56:47 +0300

    update errord and main

--- Src/main.c
+++ Src/main.c
@@ -6,6 +6,18 @@ int Frontend_DumpTokens(const char *fileName);
 int Frontend_Main(const char *fileName);
 int  Backend_Main(const char *fileName);
 
+/*
+TODO:
+
+1) add input/output system
+2) Add error handler
+3) Add machine code level
+4) Refactor Tokenizer_RecognizeOperator
+5) Refactor parser.c
+6) Refactor back-end/front-end link
+7) Add middle-end
+*/
+
 int main(int argc, char **argv)
 {
 	if (argc < 2) {
@@ -16,5 +28,6 @@ int main(int argc, char **argv)
 	}
 	Frontend_Main(argv[1]);
 	Backend_Main(argv[1]);
+
 	return 0;
 }
--- Src/token.c
+++ Src/token.c
@@ -33,7 +33,7 @@ bool IsKeyword(const char *tokenString, int keywordLength)
 enum Token_Code GetKeyword(const char *tokenString)
 {
     for (int kw = 0; kw < KEYWORDS_NUMBER; kw++) {
-        if (strncmp(tokenString, reservedKeywords[kw], strlen(reservedKeywords[kw])) == 0) {
+        if (strncmp(tokenString, reservedKeywords[kw], strlen(reservedKeywords[kw])) == 0) {    
             // all keywords start from 100 and in reservedKeywords
             // array they are placed in an appropriate order
             return 100 + kw;
