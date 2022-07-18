09cbc851d1118c31de63a4ed8f489554a467756c
XDLoLiK <66733765+XDLoLiK@users.noreply.github.com>
2022-07-18 22:08:16 +0300

    Update structures

--- Src/Structures/hashtable.c
+++ Src/Structures/hashtable.c
@@ -1,132 +1,127 @@
 #include "Structures/hashtable.h"
 
-struct HashTable *HashTable_New(size_t capacity)
+struct HashTable *HashTable_New(size_t capacity, size_t keySize, size_t valueSize)
 {
     struct HashTable *ht = calloc(1, sizeof (struct HashTable));
     if (ht == NULL) {
         return ht;
     }
 
-    ht->data  = calloc(capacity, sizeof (struct HashTableElem));
-    ht->state = calloc(capacity, sizeof (enum   HashTableState));
+    ht->data  = calloc(capacity, sizeof (struct HashTable_Elem));
+    ht->state = calloc(capacity, sizeof (enum   HashTable_State));
     if (ht->data == NULL || ht->state == NULL) {
-        free(ht->data);
-        free(ht->state);
-        free(ht);
-        return NULL;
+        return HashTable_Delete(ht);
     }
 
-    ht->capacity = capacity;
+    ht->capacity  = capacity;
+    ht->keySize   = keySize;
+    ht->valueSize = valueSize;
     return ht;
 }
 
-int HashTable_Insert(struct HashTable *ht, char *identifier, char *scope, int value)
+int HashTable_Insert(struct HashTable *ht, void *key, void *value)
 {
     if ((ht == NULL) || (ht->size + ht->size >= ht->capacity)) {
          return 1;
     }
 
-    size_t elem_hash = HashTable_MainHash(identifier) % ht->capacity;
-    size_t inc = HashTable_SecondaryHash(identifier);
+    size_t hash = HashTable_MainHash(ht, key) % ht->capacity;
+    size_t inc  = HashTable_SecondaryHash(ht, key);
 
-    for (size_t tries = 0; tries < ht->capacity; elem_hash = (elem_hash + inc) % ht->capacity, tries++) {
-        switch (ht->state[elem_hash]) {
-            case HT_STATE_Deleted: {
-                [[fallthrough]];
-            }
-            case HT_STATE_Empty: {
-                ht->data[elem_hash].value      = value;
-                ht->data[elem_hash].identifier = calloc(strlen(identifier) + 1, sizeof (char));
-                ht->data[elem_hash].scope      = calloc(strlen(scope)      + 1, sizeof (char));
-                strcpy(ht->data[elem_hash].identifier, identifier);
-                strcpy(ht->data[elem_hash].scope, scope);
-                ht->state[elem_hash] = HT_STATE_Busy;
+    for (size_t tries = 0; tries < ht->capacity; hash = (hash + inc) % ht->capacity, tries++) {
+        switch (ht->state[hash]) {
+            case HT_STATE_Deleted:
+                __attribute__((fallthrough));
+
+            case HT_STATE_Empty:
+                ht->data[hash].key   = calloc(1, ht->keySize);
+                ht->data[hash].value = calloc(1, ht->valueSize);
+                if (ht->data[hash].key == NULL || ht->data[hash].value == NULL) {
+                    free(key);
+                    free(value);
+                    return 1;
+                }
+                memcpy(ht->data[hash].key,   key,   ht->keySize);
+                memcpy(ht->data[hash].value, value, ht->valueSize);
+                ht->state[hash] = HT_STATE_Busy;
                 ht->size++;
                 return 0;
-            }
-            case HT_STATE_Busy: {
-                if (strcmp(identifier, ht->data[elem_hash].identifier) != 0) {
+
+            case HT_STATE_Busy:
+                if (memcmp(key, ht->data[hash].key, ht->keySize) != 0) {
                     continue;
                 }
-                free(ht->data[elem_hash].scope);
-                ht->data[elem_hash].scope = calloc(strlen(scope) + 1, sizeof (char));
-                strcpy(ht->data[elem_hash].scope, scope);
-                ht->data[elem_hash].value = value;
+                memcpy(ht->data[hash].value, value, ht->valueSize);
                 return 0;
-            }
-            default: {
+
+            default:
                 return 1;
-            }
         }
     }
 
     return 1;
 }
 
-size_t HashTable_Find(struct HashTable *ht, char *identifier, char *scope)
+void *HashTable_Find(struct HashTable *ht, void *key)
 {
     if (ht == NULL) {
          return HT_NOT_FOUND;
     }
 
-    size_t elem_hash = HashTable_MainHash(identifier) % ht->capacity;
-    size_t inc = HashTable_SecondaryHash(identifier);
+    size_t hash = HashTable_MainHash(ht, key) % ht->capacity;
+    size_t inc  = HashTable_SecondaryHash(ht, key);
 
-    for (size_t tries = 0; tries < ht->capacity; elem_hash = (elem_hash + inc) % ht->capacity, tries++) {
-        switch (ht->state[elem_hash]) {
-            case HT_STATE_Deleted: {
+    for (size_t tries = 0; tries < ht->capacity; hash = (hash + inc) % ht->capacity, tries++) {
+        switch (ht->state[hash]) {
+            case HT_STATE_Deleted:
                 continue;
-            }
-            case HT_STATE_Empty: {
+
+            case HT_STATE_Empty:
                 return HT_NOT_FOUND;
-            }
-            case HT_STATE_Busy: {
-                if (strcmp(identifier, ht->data[elem_hash].identifier) != 0 ||
-                    strcmp(scope,      ht->data[elem_hash].scope)      != 0) {
+
+            case HT_STATE_Busy:
+                if (memcmp(key, ht->data[hash].key, ht->keySize) != 0) {
                     continue;
                 }
-                return elem_hash;
-            }
-            default: {
+                return ht->data[hash].value;
+
+            default:
                 return HT_NOT_FOUND;
-            }
         }
     }
 
     return HT_NOT_FOUND;
 }
 
-int HashTable_Erase(struct HashTable *ht, char *identifier, char *scope)
+int HashTable_Erase(struct HashTable *ht, void *key)
 {
     if (ht == NULL) {
         return 1;
     }
 
-    size_t elem_hash = HashTable_MainHash(identifier) % ht->capacity;
-    size_t inc = HashTable_SecondaryHash(identifier);
+    size_t hash = HashTable_MainHash(ht, key) % ht->capacity;
+    size_t inc  = HashTable_SecondaryHash(ht, key);
 
-    for (size_t tries = 0; tries < ht->capacity; elem_hash = (elem_hash + inc) % ht->capacity, tries++) {
-        switch (ht->state[elem_hash]) {
-            case HT_STATE_Deleted: {
+    for (size_t tries = 0; tries < ht->capacity; hash = (hash + inc) % ht->capacity, tries++) {
+        switch (ht->state[hash]) {
+            case HT_STATE_Deleted:
                 continue;
-            }
-            case HT_STATE_Empty: {
+
+            case HT_STATE_Empty:
                 return 1;
-            }
-            case HT_STATE_Busy: {
-                if (strcmp(identifier, ht->data[elem_hash].identifier) != 0 ||
-                    strcmp(scope,      ht->data[elem_hash].scope)      != 0) {
+
+            case HT_STATE_Busy:
+                if (memcmp(key, ht->data[hash].key, ht->keySize) != 0) {
                     continue;
                 }
-                free(ht->data[elem_hash].identifier);
-                free(ht->data[elem_hash].scope);
-                ht->state[elem_hash] = HT_STATE_Deleted;
+                free(ht->data[hash].key);
+                free(ht->data[hash].value);
+                ht->state[hash] = HT_STATE_Deleted;
                 ht->size--;
                 return 0;
-            }
-            default: {
+
+            default:
                 return 1;
-            }
         }
     }
 
@@ -147,43 +142,43 @@ struct HashTable *HashTable_Delete(struct HashTable *ht)
         return ht;
     }
     
-    for (size_t i = 0; i < ht->capacity; ++i) {
-        if (ht->state[i] == HT_STATE_Busy) {
-            free(ht->data[i].scope);
-            free(ht->data[i].identifier);            
-        }
+    if (ht->data != NULL && ht->state != NULL) {
+        for (size_t i = 0; i < ht->capacity; ++i) {
+            free(ht->data[i].key);
+            free(ht->data[i].value);
+        }   
     }
+    
     free(ht->data);
     free(ht->state);
     free(ht);
-
     return NULL;
 }
 
 // djb2
-size_t HashTable_MainHash(char *str)
+size_t HashTable_MainHash(struct HashTable const *ht, void *key)
 {
     size_t hash = 5381;
-    int cur_symbol = 0;
     
-    while ((cur_symbol = *str++)) {
-        hash = ((hash << 5) + hash) + cur_symbol;
+    for (size_t i = 0; i < ht->keySize; i++) {
+        hash = ((hash << 5) + hash) + *((char *)key);
+        key = ((char *)key) + 1;
     }
     
     return hash;
 }
 
-size_t HashTable_SecondaryHash(char *str)
+size_t HashTable_SecondaryHash(struct HashTable const *ht, void *key)
 {
     size_t p = 31;
     size_t m = 1e9 + 9;
     size_t hash = 0;
     size_t p_pow = 1;
     
-    int cur_symbol = 0;
-    while ((cur_symbol = *str++)) {
-        hash = (hash + (cur_symbol - 'a' + 1) * p_pow) % m;
+    for (size_t i = 0; i < ht->keySize; i++) {
+        hash = (hash + (*((char *)key) - 'a' + 1) * p_pow) % m;
         p_pow = (p_pow * p) % m;
+        key = ((char *)key) + 1;
     }
     
     return hash;
