63137a80f7cd8cce73293237fb2ce3d86c1cd7a8
XDLoLiK <66733765+XDLoLiK@users.noreply.github.com>
2022-07-18 22:05:36 +0300

    Update structures

--- Inc/Structures/hashtable.h
+++ Inc/Structures/hashtable.h
@@ -3,45 +3,42 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <string.h>
-#include <limits.h>
-#include <inttypes.h>
+#include <memory.h>
 
-#define HT_NOT_FOUND SIZE_MAX
-#define FUNCTION     -1
+#define HT_NOT_FOUND NULL
 
-enum HashTableState {
+enum HashTable_State {
     HT_STATE_Empty   = 0,
     HT_STATE_Deleted = 1,
     HT_STATE_Busy    = 2,
 };
 
-struct HashTableElem {
-    char *identifier;
-    char *scope;
-    int value;
+struct HashTable_Elem {
+    void *key;
+    void *value;
 };
 
 struct HashTable {
     size_t capacity;
     size_t size;
-    struct HashTableElem *data;
-    enum HashTableState *state;
+    size_t keySize;
+    size_t valueSize;
+    struct HashTable_Elem *data;
+    enum HashTable_State *state;
 };
 
 // CD
-struct HashTable *HashTable_New(size_t capacity);
+struct HashTable *HashTable_New(size_t capacity, size_t keySize, size_t valueSize);
 struct HashTable *HashTable_Delete(struct HashTable *ht);
 
 // RU
 size_t HashTable_Size(struct HashTable const *ht);
-size_t HashTable_Find(struct HashTable *ht, char *identifier, char *scope);
-int HashTable_Erase(struct HashTable *ht, char *identifier, char *scope);
-int HashTable_Insert(struct HashTable *ht, char *identifier, char *scope, int value);
+void *HashTable_Find(struct HashTable *ht, void *key);
+int HashTable_Erase(struct HashTable *ht, void *key);
+int HashTable_Insert(struct HashTable *ht, void *key, void *value);
 
 // HASH
-size_t HashTable_MainHash(char *str);
-size_t HashTable_SecondaryHash(char *str);
+size_t HashTable_MainHash(struct HashTable const *ht, void *key);
+size_t HashTable_SecondaryHash(struct HashTable const *ht, void *key);
 
 #endif // Man_HASH_TABLE_H
